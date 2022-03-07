bdbe2f8f0377cae620af58903fb8fb29d614a926
Kai Bechdel <kbechdel8@gmail.com>
2022-03-07 12:22:05 -0500

    layout for hero section

--- src/components/Hero/HeroSection.js
+++ src/components/Hero/HeroSection.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import styles from './Hero.module.css'
-import { Button, Flex, Box } from '@chakra-ui/react'
+import { Button, Flex, Box, Heading } from '@chakra-ui/react'
 
 export default function HeroSection() {
     return (
-        <section >
-            <Flex alignContent="center" justifyContent="center" >
-                <div>
-                    <h1>Greetings, I'm Kai</h1>
+        <section id={styles.heroSection}>
+            <Flex alignItems="center" justifyContent="center" >
+                <div className={styles.box}>
+                    <Heading>Greetings, I'm Kai</Heading>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque libero nisi,
                         asperiores numquam, molestias nesciunt quaerat sequi, repellat ab cum vel!
                         Nisi doloremque ratione pariatur eos rem? Maxime, quos ab!</p>
-                    <div>Contact</div>
                     <Button colorScheme='blue'>Get in touch</Button>
                 </div>
 
-                <div>
-                    <img src="/images/computer.png" alt="" />
+                <div className={styles.box}>
+                    <img className={styles.heroImg} src="/images/computer.png" alt="" />
                 </div>
             </Flex>
 
 
 
-        </section>
+        </section >
     )
 }
