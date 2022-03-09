3e8f44040840f58f17f11574d6fa1ff0b252cdff
Kai Bechdel <kbechdel8@gmail.com>
2022-03-08 23:35:44 -0500

    basic project layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import styles from './Project.module.css'
 import '../../App.css'
+import { Heading } from '@chakra-ui/react'
 
 export default function Projects() {
     return (
 
         <section id='projects'>
-            Projects
-        </section>
+            <Heading size="lg">Projects</Heading>
+
+
+            {/* autoPlay loop muted */}
+            <div className={styles.firstRow}>
+                <div className={styles.project_lg}>
+                    <video className={styles.video}>
+                        <source src="images/macbook-mock.mp4" type='video/mp4' />
+                    </video>
+                </div>
+                <div className={styles.project_sm}>
+                    <img className={styles.video} src="images/floating-iphone.jpg" alt="" />
+                </div>
+            </div>
+
+            <div className={styles.secondRow}>
+                <div className={styles.project_sm}>
+                    <img className={styles.video} src="images/floating-iphone.jpg" alt="" />
+                </div>
+                <div className={styles.project_lg}>
+                    <video className={styles.video}>
+                        <source src="images/macbook-mock.mp4" type='video/mp4' />
+                    </video>
+                </div>
+
+            </div>
+
+            {/* <div className={styles.secondRow}>
+                <div className={styles.project_sm}>
+                    <img src="" alt="" />
+                </div>
+                <div className={styles.project_lg}>
+                    <img src="" alt="" />
+                </div>
+            </div> */}
+
+        </section >
     )
 }
