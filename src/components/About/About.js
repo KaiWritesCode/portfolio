a349ef386fe13d3d559cc641671246142b68ee21
Kai Bechdel <kbechdel8@gmail.com>
2022-03-09 23:01:41 -0500

    deleted large .mov file

---
+++ src/components/About/About.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styles from './About.module.css'
+
+export default function About() {
+
+    const webDevCard = (
+        <div className={styles.card}>
+            <div className={styles.cardTop}>
+                <img src="images/icons/react.png" width="50" alt="" />
+                <div className={styles.cardHeader}>Website Development</div>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, necessitatibus.
+                Ipsum voluptatibus optio temporibus iste at consectetur repellendus? Exercitationem!</p>
+        </div>
+    )
+
+
+    return (
+        <section id="about" >
+            <h2 size="lg">What can I do?</h2>
+
+            <div className={styles.flexCenter}>
+
+                <div className={styles.leftCol}>
+                    <div className={styles.cardContainer}>
+                        {webDevCard}
+                        {webDevCard}
+                    </div>
+                    <div className={styles.skillsDiv}>
+                        <div className={styles.flexBox}>
+                            <img src="images/icons/html.png" width="50" alt="" />
+                            <img src="images/icons/css.png" width="50" alt="" />
+                            <img src="images/icons/javascript.png" width="50" alt="" />
+                            <img src="images/icons/react.png" width="50" alt="" />
+                            <img src="images/icons/typescript.png" width="50" alt="" />
+                            <img src="images/icons/sass.png" width="50" alt="" />
+                        </div>
+
+                        <div className={styles.flexBox}>
+                            <img src="images/icons/firebase.png" width="50" alt="" />
+                            <img src="images/icons/git.png" width="50" alt="" />
+                            <img src="images/icons/github.png" width="50" alt="" />
+                            <img src="images/icons/photoshop.png" width="50" alt="" />
+                            <img src="images/icons/node.png" width="50" alt="" />
+                            <img src="images/icons/mysql.png" width="50" alt="" />
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+                <div className={styles.rightCol}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur in similique perferendis consequatur quidem veniam quia, natus asperiores repellat nulla eveniet aliquid et.</p>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Conse dignissimos dolor. Cum dolorum ad, deserunt tempore natus, veniam labore quidem mollitia est aliquid debitis nesciunt rem fuga consequatur!</p>
+                    <p> Minima atque vitae conseqe! Cumque obcaecati recusandae quo rerum velit odio alias quisquam tenetur expedita libero perspiciatis iusto, quod, in ducimus minima facere porro!</p>
+                    <p> Minima atque vitae conseqe! Cumque obcaecati recusandae quo rerum velit odio alias quisquam tenetur expedita libero perspiciatis iusto, quod, in ducimus minima facere porro!</p>
+                    <p> Minima atque vitae conseqe! Cumque obcaecati recusandae quo rerum velit odio alias quisquam tenetur expedita libero perspiciatis iusto, quod, in ducimus minima facere porro!</p>
+
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
