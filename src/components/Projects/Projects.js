e2841a2de908d564d6d23ef2a9e718becee5784f
Kai Bechdel <kbechdel8@gmail.com>
2022-03-16 15:58:03 -0400

    installed framer motion package

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,41 +1,52 @@
 import React from 'react'
 import styles from './Project.module.css'
 import '../../App.css'
+import { motion, Variants } from "framer-motion"
 
 export default function Projects() {
+
+
     const modalOne = (
         <div className={styles.modal}>
             <div className='flex-center'>
-                <a href='https://github.com/TheRealKai/crypto-stats' className="light-btn btn">See Code</a>
-                <a href='https://therealkai.github.io/crypto-stats/' className="light-btn btn">See Live</a>
+                <a target="_blank" rel="noreferrer" href='https://github.com/TheRealKai/crypto-stats' className="light-btn btn">See Code</a>
+                <a target="_blank" rel="noreferrer" href='https://therealkai.github.io/crypto-stats/' className="light-btn btn">See Live</a>
             </div>
         </div>
     )
     const modalTwo = (
         <div className={styles.modal}>
             <div className='flex-center'>
-                <a href='https://github.com/TheRealKai/nft-spotter' className="light-btn btn">See Code</a>
-                <a href='https://therealkai.github.io/nft-spotter/' className="light-btn btn">See Live</a>
+                <a target="_blank" rel="noreferrer" href='https://github.com/TheRealKai/nft-spotter' className="light-btn btn">See Code</a>
+                <a target="_blank" rel="noreferrer" href='https://therealkai.github.io/nft-spotter/' className="light-btn btn">See Live</a>
             </div>
         </div>
     )
     const modalThree = (
         <div className={styles.modal}>
             <div className='flex-center'>
-                <a href='https://github.com/TheRealKai/discord-chat' className="light-btn btn">See Code</a>
-                <a href='https://therealkai.github.io/discord-chat/' className="light-btn btn">See Live</a>
+                <a target="_blank" rel="noreferrer" href='https://github.com/TheRealKai/discord-chat' className="light-btn btn">See Code</a>
+                <a target="_blank" rel="noreferrer" href='https://therealkai.github.io/discord-chat/' className="light-btn btn">See Live</a>
             </div>
         </div>
     )
+
+
+
 
     return (
 
         <section id='projects'>
             <h2 size="lg">Projects</h2>
 
-
             <div className={styles.container}>
-                <div className={styles.row}>
+                <motion.div
+                    className={styles.row}
+                    initial={{ x: 1000, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ ease: [0.6, 0.2, 0.1, 1], duration: 1.2, delay: 1 }}
+                >
                     <div className={styles.imageDiv}>
                         <img className={styles.image} src="images/mockups/cryptostats.png" alt="Cryptostats project" />
                         {modalOne}
@@ -55,10 +66,15 @@ export default function Projects() {
                             <img src="images/icons/react-router.png" width="40" title='React router' alt="React router" />
                         </div>
                     </div>
-                </div>
+                </motion.div>
 
-
-                <div className={styles.row}>
+                <motion.div
+                    className={styles.row}
+                    initial={{ x: -1000, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ ease: [0.6, 0.2, 0.1, 1], duration: 1.3, delay: 1 }}
+                >
                     <div className={styles.imageDiv}>
                         <img className={`${styles.image} ${styles.nftSpotter}`} src="images/mockups/nft-spotter-mockup.png" alt="NFT Spotter project" />
                         {modalTwo}
@@ -77,9 +93,15 @@ export default function Projects() {
                             <img src="images/icons/react-router.png" width="40" title='React router' alt="React router" />
                         </div>
                     </div>
-                </div>
+                </motion.div>
 
-                <div className={styles.row}>
+                <motion.div
+                    className={styles.row}
+                    initial={{ x: 1000, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    viewport={{ once: true }}
+                    transition={{ ease: [0.6, 0.2, 0.1, 1], duration: 1, delay: 1 }}
+                >
                     <div className={styles.imageDiv}>
                         <img className={`${styles.image} ${styles.discord}`} src="images/mockups/discord-iphone.png" alt="Discord app project" />
                         {modalThree}
@@ -102,7 +124,7 @@ export default function Projects() {
                     </div>
 
 
-                </div>
+                </motion.div>
 
             </div>
 
